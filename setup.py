6a5b141ee79d76d52f48622891dcebafb3a225ba
Zuhaib M Siddique <zuhaib@hipchat.com>
2014-05-14 17:16:26 -0700

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import sys, os
 
 setup(name='txes',
-      version='0.3.4',
+      version='0.3.5',
       description="Twisted interface to elasticsearch",
       long_description="""\
 """,
