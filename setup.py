8a82fe89f7e4aeb5859d3d23378d1ebceef8747a
Lex Toumbourou <lextoumbourou@gmail.com>
2014-11-30 12:18:33 +1100

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -1,23 +1,21 @@
 from setuptools import setup
+from pip.req import parse_requirements
+
+install_reqs = parse_requirements('./requirements.txt')
+reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
-    name='txes',
-    version='0.3.7',
-    description="Twisted interface to elasticsearch",
+    name='txes2',
+    version='0.1.0',
+    description="An Elasticsearch client for Twisted",
     classifiers=[],
     keywords='twisted elasticsearch',
-    author='Jason K\xc3\xb6lker',
-    author_email='jason@koelker.net',
-    url='https://github.com/jkoelker/txes',
+    author='Lex Toumbourou',
+    author_email='lextoumbourou@gmail.com',
+    url='https://github.com/lextoumbourou/txes2',
     license='BSD License',
-    packages=['txes'],
+    packages=['txes2'],
     include_package_data=True,
     zip_safe=False,
-    install_requires=[
-        "twisted",
-        "anyjson"
-    ],
-    entry_points="""
-    # -*- Entry points: -*-
-    """,
+    install_requires=reqs
 )
