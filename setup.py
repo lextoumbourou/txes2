35c44c8ef6560571bbe37d8ae54f068c09e66e8c
Lex Toumbourou <lextoumbourou@gmail.com>
2014-12-01 17:48:46 +1100

    Fixed requirements path.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 from pip.req import parse_requirements
 
-install_reqs = parse_requirements('./requirements.txt')
+install_reqs = parse_requirements('requirements.txt')
 reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
