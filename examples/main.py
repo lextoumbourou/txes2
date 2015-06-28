17c1d29830c210ef21394e1844bc5299e3713ef8
Lex Toumbourou <lextoumbourou@gmail.com>
2015-06-28 20:38:32 +1000

    Make comments easier to read.

--- examples/main.py
+++ examples/main.py
@@ -11,36 +11,25 @@ def example():
     index = 'main'
     doc_type = 'person'
 
-    ########################
     # Setup connection
-    ########################
     es = ElasticSearch('127.0.0.1:9200')
 
-    ########################
     # Index a document
-    ########################
     document = {'name': 'Travis Bickle', 'tagline': 'You talking to me?'}
     yield es.index(document, doc_type=doc_type, index=index, id=123)
 
-    ########################
     # Retrieve a document
-    ########################
     result = yield es.get(index=index, doc_type=doc_type, id=123)
     pprint(result)
 
-    ########################
     # Refresh an index
-    ########################
     yield es.refresh(index)
 
-    ########################
     # Perform a query
-    ########################
     query = {'query': {'match': {'name': 'Travis'}}}
     results = yield es.search(query, doc_type=doc_type, index=index)
     pprint(results)
 
-    ########################
     # Perform a scan
     ########################
     query = {'query': {'match': {'name': 'Travis'}}}
