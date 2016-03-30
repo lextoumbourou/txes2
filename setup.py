b05fd9d99644eee0c1214e1e84ead3f868b4d7e8
Lex Toumbourou <lextoumbourou@gmail.com>
2016-03-30 13:10:38 +1100

    For real this time.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='txes2',
-    version='0.3.0',
+    version='0.4.0',
     description="An Elasticsearch client for Twisted",
     keywords='twisted elasticsearch',
     author='Lex Toumbourou',
